9b4729385bf6e3d543c8f9cd49135f2a9369088d
Jonas Kvinge <jonas@jkvinge.net>
2020-02-29 05:51:44 +0100

    Update glib-networking

--- src/glib-networking.mk
+++ src/glib-networking.mk
@@ -1,30 +1,25 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
 PKG             := glib-networking
 $(PKG)_WEBSITE  := https://www.gnome.org
 $(PKG)_DESCR    := Network-related GIO modules for glib
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.54.1
-$(PKG)_CHECKSUM := eaa787b653015a0de31c928e9a17eb57b4ce23c8cf6f277afaec0d685335012f
+$(PKG)_VERSION  := 2.63.3
+$(PKG)_CHECKSUM := 6581056f351f8a311cbe40d6e77d0dd1d7f2385b910da63ec87a06074ec24aca
 $(PKG)_SUBDIR   := glib-networking-$($(PKG)_VERSION)
 $(PKG)_FILE     := glib-networking-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.gnome.org/sources/$(PKG)/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc gnutls glib
+$(PKG)_DEPS     := cc meson-conf ninja glib openssl
 
 define $(PKG)_UPDATE
-    echo 'Updates for package $(PKG) is disabled.' >&2;
-    echo $($(PKG)_VERSION)
+    $(call MXE_GET_GH_TAGS,GNOME/glib-networking) | \
+    grep -v '\([0-9]\+\.\)\{2\}9[0-9]' | \
+    $(SORT) -Vr | \
+    head -1
 endef
 
-#define $(PKG)_UPDATE
-#    $(WGET) -q -O- 'https://gitlab.gnome.org/GNOME/glib-networking/-/tags' | \
-#    $(SED) -n "s,.*glib-networking-\([0-9]\+\.[0-9]*[0-9]*\.[^']*\)\.tar.*,\1,p" | \
-#    $(SORT) -Vr | \
-#    head -1
-#endef
-
 define $(PKG)_BUILD
-    cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' \
-         $(MXE_CONFIGURE_OPTS) \
-         --without-ca-certificates
-    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)' install
+    cd '$(SOURCE_DIR)' && $(TARGET)-meson -Dgnutls=disabled -Dopenssl=enabled '$(BUILD_DIR)'
+    cd '$(BUILD_DIR)' && ninja
+    cd '$(BUILD_DIR)' && ninja install
 endef
