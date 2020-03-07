0f62345f23b5086a686f8fb2ec8926af240fe94a
Jonas Kvinge <jonas@jkvinge.net>
2020-03-07 18:32:18 +0100

    Update singleapplication

--- src/singleapplication.mk
+++ src/singleapplication.mk
@@ -4,9 +4,9 @@ PKG             := singleapplication
 $(PKG)_WEBSITE  := https://github.com/itay-grudev/SingleApplication
 $(PKG)_DESCR    := SingleApplication
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 666fd4d
-$(PKG)_CHECKSUM := 07df0507b8902f7a855edb5b5490c93befaf4e0333eb5f9b6a01920f4ef1514c
-$(PKG)_GH_CONF  := itay-grudev/SingleApplication/branches/master
+$(PKG)_VERSION  := 3.1.0a
+$(PKG)_CHECKSUM := 620908325070fda4ef1245054ce1bb6c8a813bd3ffb5cf9c6251dfb9dee266b4
+$(PKG)_GH_CONF  := itay-grudev/SingleApplication/tags, v
 $(PKG)_DEPS     := cc qtbase
 
 define $(PKG)_BUILD
