1cee9956acf70f63dae50f27d83c7b7ba11f6743
Jonas Kvinge <jonas@jkvinge.net>
2020-10-08 19:17:58 +0200

    Update jasper

--- src/jasper.mk
+++ src/jasper.mk
@@ -4,8 +4,8 @@ PKG             := jasper
 $(PKG)_WEBSITE  := https://www.ece.uvic.ca/~mdadams/jasper/
 $(PKG)_DESCR    := JasPer
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.0.20
-$(PKG)_CHECKSUM := d55843ce52afa9bfe90f30118329578501040f30d48a027459a68a962695e506
+$(PKG)_VERSION  := 2.0.22
+$(PKG)_CHECKSUM := afc4166bff29b8a0dc46ed5e8d6a208d7976fccfd0b1146e3400c8b2948794a2
 $(PKG)_GH_CONF  := mdadams/jasper/tags, version-
 $(PKG)_DEPS     := cc jpeg
 
