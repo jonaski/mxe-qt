5afcc4fd1051c689eef415c480443cf2b21b0ce5
Jonas Kvinge <jonas@jkvinge.net>
2021-03-23 01:01:38 +0100

    Update protobuf

--- src/protobuf.mk
+++ src/protobuf.mk
@@ -3,8 +3,8 @@
 PKG             := protobuf
 $(PKG)_WEBSITE  := https://github.com/google/protobuf
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.15.1
-$(PKG)_CHECKSUM := f18a40816260a9a3190a94efb0fc26270b244a2436681602f0a944739095d632
+$(PKG)_VERSION  := 3.15.6
+$(PKG)_CHECKSUM := 65e020a42bdab44a66664d34421995829e9e79c60e5adaa08282fd14ca552f57
 $(PKG)_GH_CONF  := google/protobuf/tags, v
 $(PKG)_DEPS     := cc googletest zlib $(BUILD)~$(PKG)
 $(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
