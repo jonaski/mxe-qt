12f34dbc852dc713fd19e64366cba91d554bdabe
Jonas Kvinge <jonas@jkvinge.net>
2022-02-19 16:21:00 +0100

    Update pixman

--- src/pixman.mk
+++ src/pixman.mk
@@ -1,23 +1,24 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := pixman
-$(PKG)_WEBSITE  := https://cairographics.org/
+$(PKG)_WEBSITE  := http://www.pixman.org/
+$(PKG)_DESCR    := Pixman is a low-level software library for pixel manipulation
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.33.6
-$(PKG)_CHECKSUM := 4e1e72c0ed31d10944f304976e87e6c87b441c853713eeecf115e22c23d4b17d
+$(PKG)_VERSION  := 0.40.0
+$(PKG)_CHECKSUM := 6d200dec3740d9ec4ec8d1180e25779c00bc749f94278c8b9021f5534db223fc
 $(PKG)_SUBDIR   := pixman-$($(PKG)_VERSION)
 $(PKG)_FILE     := pixman-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://cairographics.org/snapshots/$($(PKG)_FILE)
+$(PKG)_URL      := https://www.cairographics.org/releases/$($(PKG)_FILE)
 $(PKG)_URL_2    := https://xorg.freedesktop.org/archive/individual/lib/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc libpng
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://cairographics.org/snapshots/?C=M;O=D' | \
+    $(WGET) -q -O- 'https://www.cairographics.org/releases/?C=M;O=D' | \
     $(SED) -n 's,.*"pixman-\([0-9][^"]*\)\.tar.*,\1,p' | \
     head -1
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && ./configure $(MXE_CONFIGURE_OPTS)
+    cd '$(1)' && ./configure $(MXE_CONFIGURE_OPTS) --enable-libpng --disable-gtk
     $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
 endef
