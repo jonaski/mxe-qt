390873bb117ac16e176b139b8c1ae21ad0d6cb7a
Jonas Kvinge <jonas@jkvinge.net>
2022-02-19 16:19:39 +0100

    Update libpsl

--- src/libpsl.mk
+++ src/libpsl.mk
@@ -1,17 +1,18 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := libpsl
-$(PKG)_WEBSITE  := http://www.linuxfromscratch.org/blfs/view/svn/basicnet/libpsl.html
+$(PKG)_WEBSITE  := https://github.com/rockdaboot/libpsl
+$(PKG)_DESCR    := C library for the Public Suffix List
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.21.0
-$(PKG)_CHECKSUM := 41bd1c75a375b85c337b59783f5deb93dbb443fb0a52d257f403df7bd653ee12
-$(PKG)_GH_CONF  := rockdaboot/libpsl/tags, libpsl-
+$(PKG)_VERSION  := 0.21.1
+$(PKG)_CHECKSUM := ac6ce1e1fbd4d0254c4ddb9d37f1fa99dec83619c1253328155206b896210d4c
+$(PKG)_GH_CONF  := rockdaboot/libpsl/releases/latest
 $(PKG)_SUBDIR   := libpsl-$($(PKG)_VERSION)
 $(PKG)_FILE     := libpsl-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://github.com/rockdaboot/libpsl/releases/download/$(PKG)-$($(PKG)_VERSION)/$(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_DEPS     := cc
+$(PKG)_URL      := https://github.com/rockdaboot/libpsl/releases/download/$($(PKG)_VERSION)/$(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_DEPS     := cc libunistring
 
 define $(PKG)_BUILD
-    cd '$(1)' && ./configure $(MXE_CONFIGURE_OPTS)
+    cd '$(1)' && ./configure $(MXE_CONFIGURE_OPTS) --disable-rpath --disable-nls
     $(MAKE) -C '$(1)' -j '$(JOBS)' install
 endef
