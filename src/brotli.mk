339d49495dffa0731a6b0e649523f343ed59e162
Jonas Kvinge <jonas@jkvinge.net>
2022-02-20 15:58:26 +0100

    Add brotli

---
+++ src/brotli.mk
@@ -0,0 +1,16 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := brotli
+$(PKG)_WEBSITE  := https://opensource.google.com/projects/brotli
+$(PKG)_DESCR    := Brotli compression format
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 1.0.9
+$(PKG)_CHECKSUM := f9e8d81d0405ba66d181529af42a3354f838c939095ff99930da6aa9cdf6fe46
+$(PKG)_GH_CONF  := google/brotli/tags, v
+$(PKG)_DEPS     := cc
+
+define $(PKG)_BUILD
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' -DCMAKE_BUILD_TYPE='$(MXE_BUILD_TYPE)' '$(SOURCE_DIR)'
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
+endef
