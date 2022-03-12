ee68fef31b3ed78b712fbc89b9ee6062add03b15
Jonas Kvinge <jonas@jkvinge.net>
2022-03-12 23:20:05 +0100

    Qt: Use minimal native build

--- src/qt6-qttools.mk
+++ src/qt6-qttools.mk
@@ -22,20 +22,49 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    $(QT6_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)' -DCMAKE_BUILD_TYPE='$(MXE_BUILD_TYPE)' -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON -DFEATURE_assistant=OFF
+    $(QT6_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)' -DCMAKE_BUILD_TYPE='$(MXE_BUILD_TYPE)' \
+        -DQT_BUILD_EXAMPLES=OFF \
+        -DQT_BUILD_EXAMPLES_BY_DEFAULT=OFF \
+        -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON \
+        -DFEATURE_assistant=OFF \
+        -DFEATURE_designer=ON \
+        -DFEATURE_distancefieldgenerator=OFF \
+        -DFEATURE_kmap2qmap=OFF \
+        -DFEATURE_macdeployqt=OFF \
+        -DFEATURE_pixeltool=OFF \
+        -DFEATURE_qdbus=OFF \
+        -DFEATURE_qev=OFF \
+        -DFEATURE_qtattributionsscanner=OFF \
+        -DFEATURE_qtdiag=OFF \
+        -DFEATURE_qtplugininfo=OFF \
+        -DFEATURE_windeployqt=OFF \
+        -DFEATURE_linguist=ON
+
     cmake --build '$(BUILD_DIR)' -j '$(JOBS)'
     cmake --install '$(BUILD_DIR)'
 
-    cp '$(PREFIX)/$(BUILD)/qt6/libexec/moc' '$(PREFIX)/$(TARGET)/qt6/bin/moc.exe'
-    cp '$(PREFIX)/$(BUILD)/qt6/libexec/rcc' '$(PREFIX)/$(TARGET)/qt6/bin/rcc.exe'
-    cp '$(PREFIX)/$(BUILD)/qt6/libexec/uic' '$(PREFIX)/$(TARGET)/qt6/bin/uic.exe'
-    cp '$(PREFIX)/$(BUILD)/qt6/bin/lrelease' '$(PREFIX)/$(TARGET)/qt6/bin/lrelease.exe'
-    cp '$(PREFIX)/$(BUILD)/qt6/bin/lconvert' '$(PREFIX)/$(TARGET)/qt6/bin/lconvert.exe'
+    $(INSTALL) -m755 '$(PREFIX)/$(BUILD)/qt6/bin/lrelease' '$(PREFIX)/$(TARGET)/qt6/bin/lrelease.exe'
+    $(INSTALL) -m755 '$(PREFIX)/$(BUILD)/qt6/bin/lconvert' '$(PREFIX)/$(TARGET)/qt6/bin/lconvert.exe'
 
 endef
 
 define $(PKG)_BUILD_$(BUILD)
-    $(QT6_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)'
+    $(QT6_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)' \
+        -DQT_BUILD_EXAMPLES=OFF \
+        -DQT_BUILD_EXAMPLES_BY_DEFAULT=OFF \
+        -DFEATURE_assistant=OFF \
+        -DFEATURE_designer=ON \
+        -DFEATURE_distancefieldgenerator=OFF \
+        -DFEATURE_kmap2qmap=OFF \
+        -DFEATURE_macdeployqt=OFF \
+        -DFEATURE_pixeltool=OFF \
+        -DFEATURE_qdbus=OFF \
+        -DFEATURE_qev=OFF \
+        -DFEATURE_qtattributionsscanner=OFF \
+        -DFEATURE_qtdiag=OFF \
+        -DFEATURE_qtplugininfo=OFF \
+        -DFEATURE_windeployqt=OFF \
+        -DFEATURE_linguist=ON
     cmake --build '$(BUILD_DIR)' -j '$(JOBS)'
     cmake --install '$(BUILD_DIR)'
 endef
