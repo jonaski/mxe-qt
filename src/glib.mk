8e012f13daff953ec79b4169007f659842f8c218
Jonas Kvinge <jonas@jkvinge.net>
2022-07-14 01:07:56 +0200

    Update glib

--- src/glib.mk
+++ src/glib.mk
@@ -4,8 +4,8 @@ PKG             := glib
 $(PKG)_WEBSITE  := https://docs.gtk.org/glib/
 $(PKG)_DESCR    := GLib general-purpose, portable utility library
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.73.0
-$(PKG)_CHECKSUM := 3673f10515f4bcfb9ee2ce0a921a18fa359c36fab388b19819467e7b09506870
+$(PKG)_VERSION  := 2.73.2
+$(PKG)_CHECKSUM := 5f3ee36e34f4aaab393c3e3dc46fb01b32f7ead6c88d41d7f20d88a49cdef1d9
 $(PKG)_SUBDIR   := glib-$($(PKG)_VERSION)
 $(PKG)_FILE     := glib-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.gnome.org/sources/glib/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
@@ -18,8 +18,6 @@ $(PKG)_DEPS_$(BUILD) := ninja gettext libffi libiconv zlib
 define $(PKG)_UPDATE
     $(call MXE_GET_GH_TAGS,GNOME/glib) | \
     grep -v '\([0-9]\+\.\)\{2\}9[0-9]' | \
-    grep -v '2.73.1' | \
-    grep -v '2.73.2' | \
     $(SORT) -Vr | \
     head -1
 endef
@@ -40,7 +38,7 @@ define $(PKG)_BUILD
     ln -sf '$(PREFIX)/$(BUILD)/bin/glib-genmarshal'        '$(PREFIX)/$(TARGET)/bin/'
     ln -sf '$(PREFIX)/$(BUILD)/bin/glib-compile-schemas'   '$(PREFIX)/$(TARGET)/bin/'
     ln -sf '$(PREFIX)/$(BUILD)/bin/glib-compile-resources' '$(PREFIX)/$(TARGET)/bin/'
-    cd '$(SOURCE_DIR)' && '$(TARGET)-meson' --buildtype='$(MESON_BUILD_TYPE)' -Dforce_posix_threads=true '$(BUILD_DIR)'
+    cd '$(SOURCE_DIR)' && '$(TARGET)-meson' --buildtype='$(MESON_BUILD_TYPE)' -Dforce_posix_threads=true -Dtests=false '$(BUILD_DIR)'
     cd '$(BUILD_DIR)' && ninja
     cd '$(BUILD_DIR)' && ninja install
 endef
