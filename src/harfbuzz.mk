1b467fb264a22d4220bbe7441bbbcecec8a3bb4a
Jonas Kvinge <jonas@jkvinge.net>
2023-07-22 19:23:12 +0200

    harfbuzz: Switch to meson/ninja build

--- src/harfbuzz.mk
+++ src/harfbuzz.mk
@@ -7,17 +7,20 @@ $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 8.0.1
 $(PKG)_CHECKSUM := d54ca67b6a0bf732b66a343566446d7f93df2bb850133f886c0082fb618a06b2
 $(PKG)_GH_CONF  := harfbuzz/harfbuzz/releases
-$(PKG)_DEPS     := cc cairo freetype-bootstrap glib icu4c
+$(PKG)_DEPS     := cc freetype-bootstrap glib icu4c cairo libiconv
 
 define $(PKG)_BUILD
-    # mman-win32 is only a partial implementation
-    cd '$(1)' && ./autogen.sh
-    cd '$(1)' && ./configure $(MXE_CONFIGURE_OPTS) \
-        --with-glib=yes \
-        --with-icu=yes \
-        --with-freetype=yes \
-        --with-cairo=yes \
-        ac_cv_header_sys_mman_h=no
-    $(MAKE) -C '$(1)'
-    $(MAKE) -C '$(1)' install
+    cd '$(SOURCE_DIR)' && LDFLAGS='$(LDFLAGS) -liconv' '$(TARGET)-meson' \
+        --buildtype='$(MESON_BUILD_TYPE)' \
+        -Dtests=disabled \
+        -Ddocs=disabled \
+        -Dglib=enabled \
+        -Dgobject=enabled \
+        -Dcairo=enabled \
+        -Dcoretext=enabled \
+        -Dfreetype=enabled \
+        -Dicu=enabled \
+        '$(BUILD_DIR)'
+   cd '$(BUILD_DIR)' && ninja
+   cd '$(BUILD_DIR)' && ninja install
 endef
