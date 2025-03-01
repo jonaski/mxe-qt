38b9c126e22e2e12d2210778c198d7890036ba61
Jonas Kvinge <jonas@jkvinge.net>
2025-03-01 11:41:26 +0100

    Update c-ares

--- src/cares.mk
+++ src/cares.mk
@@ -4,11 +4,11 @@ PKG             := cares
 $(PKG)_WEBSITE  := https://c-ares.org/
 $(PKG)_DESCR    := c-ares is a C library for asynchronous DNS requests
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.29.0
-$(PKG)_CHECKSUM := 0b89fa425b825c4c7bc708494f374ae69340e4d1fdc64523bdbb2750bfc02ea7
+$(PKG)_VERSION  := 1.34.4
+$(PKG)_CHECKSUM := fa38dbed659ee4cc5a32df5e27deda575fa6852c79a72ba1af85de35a6ae222f
 $(PKG)_FILE     := c-ares-$($(PKG)_VERSION).tar.gz
 $(PKG)_SUBDIR   := c-ares-$($(PKG)_VERSION)
-$(PKG)_URL      := https://github.com/c-ares/c-ares/releases/download/cares-$(subst .,_,$($(PKG)_VERSION))/$($(PKG)_FILE)
+$(PKG)_URL      := https://github.com/c-ares/c-ares/releases/download/v$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc
 
 define $(PKG)_UPDATE
