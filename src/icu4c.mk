2363b8a80e25346ad7b56ec46d05da58113daacf
Jonas Kvinge <jonas@jkvinge.net>
2025-04-03 20:59:05 +0200

    Update icu4c

--- src/icu4c.mk
+++ src/icu4c.mk
@@ -4,13 +4,13 @@ PKG             := icu4c
 $(PKG)_WEBSITE  := https://icu.unicode.org/
 $(PKG)_DESCR    := ICU4C
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 76.1
+$(PKG)_VERSION  := 77.1
 $(PKG)_MAJOR    := $(word 1,$(subst ., ,$($(PKG)_VERSION)))
-$(PKG)_CHECKSUM := a2c443404f00098e9e90acf29dc318e049d2dc78d9ae5f46efb261934a730ce2
+$(PKG)_CHECKSUM := 588e431f77327c39031ffbb8843c0e3bc122c211374485fa87dc5f3faff24061
 $(PKG)_GH_CONF  := unicode-org/icu/releases/latest,release-,,,-
-$(PKG)_SUBDIR   := icu-release-$(subst .,-,$($(PKG)_VERSION))/$(PKG)
-$(PKG)_URL      := $($(PKG)_WEBSITE)/releases/download/release-$(subst .,-,$($(PKG)_VERSION))/icu4c-$(subst .,_,$($(PKG)_VERSION))-src.tgz
-$(PKG)_DEPS     := cc $(BUILD)~$(PKG) pe-util
+$(PKG)_SUBDIR   := icu/source
+$(PKG)_URL      := https://github.com/unicode-org/icu/releases/download/release-$(subst .,-,$($(PKG)_VERSION))/icu4c-$(subst .,_,$($(PKG)_VERSION))-src.tgz
+$(PKG)_DEPS     := cc $(BUILD)~$(PKG)
 
 $(PKG)_TARGETS       := $(BUILD) $(MXE_TARGETS)
 $(PKG)_DEPS_$(BUILD) :=
@@ -20,7 +20,7 @@ define $(PKG)_BUILD_$(BUILD)
     # cross build requires artefacts from native build tree
     rm -rf '$(PREFIX)/$(BUILD)/$(PKG)'
     $(INSTALL) -d '$(PREFIX)/$(BUILD)/$(PKG)'
-    cd '$(PREFIX)/$(BUILD)/$(PKG)' && '$(SOURCE_DIR)/source/configure' CC=$(BUILD_CC) CXX=$(BUILD_CXX) --enable-tests=no --enable-samples=no
+    cd '$(PREFIX)/$(BUILD)/$(PKG)' && '$(SOURCE_DIR)/configure' CC=$(BUILD_CC) CXX=$(BUILD_CXX) --enable-tests=no --enable-samples=no
     $(MAKE) -C '$(PREFIX)/$(BUILD)/$(PKG)' -j '$(JOBS)'
 
 endef
@@ -28,7 +28,7 @@ endef
 define $(PKG)_BUILD_COMMON
 
     rm -fv $(shell echo "$(PREFIX)/$(TARGET)"/{bin,lib}/{lib,libs,}icu'*'.{a,dll,dll.a})
-    cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/source/configure' $(MXE_CONFIGURE_OPTS) --with-cross-build='$(PREFIX)/$(BUILD)/$(PKG)' --enable-icu-config=no SHELL=$(SHELL) CXXFLAGS='-std=gnu++17' LIBS='-lstdc++' $($(PKG)_CONFIGURE_OPTS)
+    cd '$(BUILD_DIR)' && '$(SOURCE_DIR)/configure' $(MXE_CONFIGURE_OPTS) --with-cross-build='$(PREFIX)/$(BUILD)/$(PKG)' --enable-icu-config=no SHELL=$(SHELL) CXXFLAGS='-std=gnu++17' LIBS='-lstdc++' $($(PKG)_CONFIGURE_OPTS)
 
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1 SO_TARGET_VERSION_SUFFIX=
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1 SO_TARGET_VERSION_SUFFIX=
@@ -41,20 +41,11 @@ endef
 
 define $(PKG)_BUILD_SHARED
     $($(PKG)_BUILD_COMMON)
-    # icu4c installs its DLLs to lib/. Move them to bin/.
-    #mv -fv $(PREFIX)/$(TARGET)/lib/icu*.dll '$(PREFIX)/$(TARGET)/bin/'
 
     # stub data is icudt.dll, actual data is libicudt.dll - prefer actual
     test ! -e '$(PREFIX)/$(TARGET)/lib/libicudt$($(PKG)_MAJOR).dll' || mv -fv '$(PREFIX)/$(TARGET)/lib/libicudt$($(PKG)_MAJOR).dll' '$(PREFIX)/$(TARGET)/bin/icudt$($(PKG)_MAJOR).dll'
 
     $($(PKG)_BUILD_TEST)
-
-    # bundle test to verify deployment
-    rm -rfv '$(PREFIX)/$(TARGET)/bin/test-$(PKG)' '$(PREFIX)/$(TARGET)/bin/test-$(PKG).zip'
-    $(INSTALL) -d '$(PREFIX)/$(TARGET)/bin/test-$(PKG)'
-    cp $$($(TARGET)-peldd --all '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe') '$(PREFIX)/$(TARGET)/bin/test-$(PKG)'
-    cd '$(PREFIX)/$(TARGET)/bin' && 7za a -tzip test-$(PKG).zip test-$(PKG)
-    rm -rfv '$(PREFIX)/$(TARGET)/bin/test-$(PKG)'
 endef
 
 define $(PKG)_BUILD
